e61609858ba819a30b682969fb83a850205ee11e
victo <victo@DESKTOP-OKA1O85>
2024-01-16 20:35:36 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Entidade/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Entities
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Cidade { get; set; }
+        public string? Endereco { get; set; }
+        public int? Telefone { get; set; }
+
+        public Funcionario(string? name, string? cidade, string? endereco, int? telefone)
+        {
+            Name = name;
+            Cidade = cidade;
+            Endereco = endereco;
+            Telefone = telefone;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,298 @@
+﻿using ConsoleApp2.Entities;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp2
+{
+    public class Projeto
+    {
+        private List<Funcionario> Lista = new List<Funcionario>();
+
+        public static void Main()
+        {
+            string opcao;
+            Projeto projeto = new Projeto();
+            do
+            {
+                opcao = Menu();
+
+                switch (opcao)
+                {
+                    case "0":
+                        break;
+                    case "1":   
+                        projeto.Cadastrar();
+                        break;
+                    
+                    case "2":
+                        projeto.Alterar();
+                        break;
+                    
+                    case "3":
+                        projeto.Excluir();
+                        break;
+                    
+                    case "4":
+                        projeto.ExibirNome();
+                        break;
+                    
+                    case "5":
+                        projeto.ExibirLetra();
+                        break;
+                    
+                    case "6":
+                        projeto.ExibirTodos();
+                        break;
+
+                    case "7":
+                        projeto.ExibirID();
+                        break;
+                    default: Console.WriteLine("Opção Invalida !");
+                        Console.ReadLine();
+                        break;
+                }
+                Console.Clear();
+            } while (opcao != "0");
+        }
+
+        private static string Menu()
+        {
+            Console.Write("[1] - Cadastrar Funcionario\n" +
+                              "[2] - Alterar Funcionario\n" +
+                              "[3] - Excluir Funcionario\n" +
+                              "[4] - Exibir Funcionario por Nome\n" +
+                              "[5] - Exibir Funcionario por Letra\n" +
+                              "[6] - Exibir Todos os Funcionarios\n" +
+                              "[7] - Exibir por ID\n" +
+                              "[0] - Sair\n" +
+                              "Opção: ");
+
+            string opcao = Console.ReadLine();
+            Console.Clear();
+            return opcao;
+        }
+
+        private void Cadastrar()
+        {
+            Console.WriteLine("Digite o nome do Funcionario:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o Endereço:");
+            string endereco = Console.ReadLine();
+
+            Console.WriteLine("Digite a Cidade:");
+            string cidade = Console.ReadLine();
+
+            Console.WriteLine("Digite o Telefone:");
+            int telefone = int.Parse(Console.ReadLine());
+
+            Funcionario funcionario = new Funcionario(nome, cidade, endereco, telefone);
+
+            if (Lista.Count == 0)
+                funcionario.Id = 1;
+            else
+                funcionario.Id = Lista[Lista.Count - 1].Id + 1;
+
+            Lista.Add(funcionario);
+        }
+
+        private void Alterar()
+        {
+            try
+            {
+                Console.WriteLine("Digite o ID do Funcionario que deseja alterar os dados:");
+                int id = int.Parse(Console.ReadLine());
+
+                Funcionario auxiliar = Lista.FirstOrDefault(x => x.Id == id);
+
+                Console.WriteLine($"Nome:{auxiliar.Name}\n" +
+                                  $"Endereço:{auxiliar.Endereco}\n" +
+                                  $"Cidade:{auxiliar.Cidade}\n" +
+                                  $"Telefone:{auxiliar.Telefone}\n\n" +
+                                  $"Desejar alterar os dados ?[S/N]\n" +
+                                  $"Opção: ");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+                if (opcao.Equals("S"))
+                {
+                    string nome, endereco, cidade;
+                    int telefone;
+
+                    do
+                    {
+                        Console.WriteLine("\n\nDigite o nome do Funcionario:");
+                        nome = Console.ReadLine();
+
+                        Console.WriteLine("Digite o Endereço:");
+                        endereco = Console.ReadLine();
+
+                        Console.WriteLine("Digite a Cidade:");
+                        cidade = Console.ReadLine();
+
+                        Console.WriteLine("Digite o Telefone:");
+                        telefone = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"Nome:{nome}\n" +
+                                      $"Endereço:{endereco}\n" +
+                                      $"Cidade:{cidade}\n" +
+                                      $"Telefone:{telefone}\n\n" +
+                                      $"Confirma os novos dados?[S/N]\n" +
+                                      $"Opção");
+
+                        opcao = Console.ReadLine().ToUpper();
+
+                    } while (opcao == "N");
+
+                    auxiliar.Name = nome;
+                    auxiliar.Endereco = endereco;
+                    auxiliar.Cidade = cidade;
+                    auxiliar.Telefone = telefone;
+
+                    Console.WriteLine("Dados alterados com Sucesso");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        private void Excluir()
+        {
+            try
+            {
+                Console.WriteLine("Digite o ID do Funcionario que deseja excluir os dados:");
+                int id = int.Parse(Console.ReadLine());
+
+                Funcionario auxiliar = Lista.FirstOrDefault(x => x.Id == id);
+
+                Console.WriteLine($"Nome:{auxiliar.Name}\n" +
+                                  $"Endereço:{auxiliar.Endereco}\n" +
+                                  $"Cidade:{auxiliar.Cidade}\n" +
+                                  $"Telefone:{auxiliar.Telefone}\n\n" +
+                                  $"Desejar excluir os dados ?[S/N]\n" +
+                                  $"Opção: ");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+                if (opcao.Equals("S"))
+                {
+                    Lista.Remove(auxiliar);
+                    Console.Write("Remoção bem sucedida");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void ExibirLetra()
+        {
+            if (Lista.Count > 0)
+            {
+                Console.Write("Buscar funcionario que tenha a letra: ");
+                string letra = Console.ReadLine();
+
+                List<Funcionario> listaBusca = Lista.Where(x => x.Name.Contains(letra)).ToList();
+                
+                if (listaBusca.Count > 0)
+                {
+                    foreach (var lista in listaBusca)
+                        Console.WriteLine($"ID:{lista.Id}\n" +
+                                            $"Funcionario:{lista.Name}\n" +
+                                            $"Endereco:{lista.Endereco}\n" +
+                                            $"Cidade:{lista.Cidade}\n" +
+                                            $"Telefone:{lista.Telefone}\n");
+                    Console.WriteLine("Aperte qualquer tecla para sair");
+                }
+                else
+                    Console.WriteLine("Nenhum dado encontrado.\nPressione qualquer tecla para sair");
+            }
+            else
+                Console.WriteLine("Lista Vazia.\n Aperte qualquer tecla para sair.");
+
+            Console.ReadLine();
+        }
+
+        private void ExibirNome()
+        {
+            if(Lista.Count>0)
+            {
+                Console.Write("Buscar funcionario que tenha o Nome: ");
+                string Nome = Console.ReadLine();
+
+                List<Funcionario> listaBusca = Lista.Where(x => x.Name.Equals(Nome, StringComparison.OrdinalIgnoreCase)).ToList();
+
+                if (listaBusca.Count > 0)
+                {
+                    foreach (var lista in listaBusca)
+                        Console.WriteLine($"ID:{lista.Id}\n" +
+                                            $"Funcionario:{lista.Name}\n" +
+                                            $"Endereco:{lista.Endereco}\n" +
+                                            $"Cidade:{lista.Cidade}\n" +
+                                            $"Telefone:{lista.Telefone}\n");
+                    Console.WriteLine("Aperte qualquer tecla para sair");
+                }
+                else
+                    Console.WriteLine("Nenhum dado encontrado.\nPressione qualquer tecla para sair");
+
+            }
+            else
+                Console.WriteLine("Lista Vazia.\n Aperte qualquer tecla para sair.");
+
+            Console.ReadLine();
+        }
+        private void ExibirTodos()
+        {
+            if(Lista.Count > 0)
+            {
+                foreach (var lista in Lista)
+                    Console.WriteLine($"ID:{lista.Id}\n" +
+                                      $"Funcionario:{lista.Name}\n" +
+                                      $"Endereco:{lista.Endereco}\n" +
+                                      $"Cidade:{lista.Cidade}\n" +
+                                      $"Telefone:{lista.Telefone}\n");
+                Console.WriteLine("Aperte qualquer tecla para sair");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Lista vazia");
+                Console.ReadLine();
+            }
+            
+        }
+
+        private void ExibirID()
+        {
+            if (Lista.Count > 0)
+            {
+                Console.Write("Buscar funcionario que tenha o ID: ");
+                int Id = int.Parse(Console.ReadLine());
+
+                Funcionario busca = Lista.FirstOrDefault(x => x.Id==Id);
+
+                if (busca!=null)
+                {
+                    Console.WriteLine($"ID:{busca.Id}\n" +
+                                            $"Funcionario:{busca.Name}\n" +
+                                            $"Endereco:{busca.Endereco}\n" +
+                                            $"Cidade:{busca.Cidade}\n" +
+                                            $"Telefone:{busca.Telefone}\n");
+                    Console.WriteLine("Aperte qualquer tecla para sair");
+                }
+                else
+                    Console.WriteLine("Nenhum dado encontrado.\nPressione qualquer tecla para sair");
+
+            }
+            else
+                Console.WriteLine("Lista Vazia.\n Aperte qualquer tecla para sair.");
+
+            Console.ReadLine();
+        }
+    }
+
+}
